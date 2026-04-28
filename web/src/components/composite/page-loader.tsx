4cc836eeaefd55c4d7111cc056ed51fdc0c7f4c6
Shihab Mahamud <shihab4t@gmail.com>
2026-04-28 13:10:09 +0600

    page loader

--- web/src/components/composite/page-loader.tsx
+++ web/src/components/composite/page-loader.tsx
@@ -1,9 +1,9 @@
-import { LoaderCircleIcon } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
 
-export function PageLoader() {
+export const PageLoader = () => {
     return (
         <div className="flex min-h-svh items-center justify-center">
-            <LoaderCircleIcon className="size-6 animate-spin text-muted-foreground" />
+            <Loader2 className="size-52 animate-spin text-primary" />
         </div>
     );
-}
+};
