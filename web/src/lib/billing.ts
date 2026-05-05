e71c2353b22e84e82fa6ac133117cb9cea50bbff
Shihab Mahamud <shihab4t@gmail.com>
2026-05-05 15:51:31 +0600

    billing

--- web/src/lib/billing.ts
+++ web/src/lib/billing.ts
@@ -11,8 +11,11 @@ export const getAlreadyCanceled = (subscription: TSubscription) => {
     return subscription.status === 'canceled';
 };
 
-export const getScheduledToCancel = (subscription: TSubscription | null | undefined): boolean => {
-    if (!subscription) return false;
+export const getScheduledToCancel = (
+    subscription: TSubscription | null,
+    user: TUser | null,
+): boolean => {
+    if (!subscription) return !!user?.subscription_cancel_at;
     return !!subscription.cancel_at || !!subscription.cancel_at_period_end;
 };
 
@@ -20,10 +23,8 @@ export const getIsFree = (user: TUser): boolean => {
     return (user.plan ?? 'free') === 'free';
 };
 
-export const getCanCancel = (
-    user: TUser,
-    subscription: TSubscription | undefined | null,
-): boolean => {
+export const getCanCancel = (user: TUser | null, subscription: TSubscription | null): boolean => {
+    if (!user) return false;
     if (getIsFree(user)) return false;
     const stripeStatus = subscription?.status as TSubscriptionStatus | undefined;
     const status = (stripeStatus ?? user.subscription_status) as TSubscriptionStatus | undefined;
