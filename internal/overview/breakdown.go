62c94c93bb9dc1598cafad082c40308f4fa1533c
Shihab Mahamud <shihab4t@gmail.com>
2026-05-07 16:49:29 +0600

    refactor(overview): generate breakdown query from field table
    Eliminates 6x repeated CTE boilerplate; adding a new breakdown
    field now requires one line in breakdownFields.

--- internal/overview/breakdown.go
+++ internal/overview/breakdown.go
@@ -1,6 +1,9 @@
 package overview
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/pocketbase/dbx"
 )
 
@@ -18,34 +21,31 @@ type breakdownData struct {
 	Languages []breakdownItem `json:"languages"`
 }
 
+var breakdownFields = [][2]string{
+	{"country", "country_name"},
+	{"device", "device"},
+	{"referrer", "referrer"},
+	{"browser", "browser"},
+	{"os", "os"},
+	{"language", "language"},
+}
+
 func fetchBreakdown(db dbx.Builder, uid string) breakdownData {
+	parts := make([]string, len(breakdownFields))
+	for i, f := range breakdownFields {
+		parts[i] = fmt.Sprintf(
+			"SELECT * FROM (SELECT '%s' AS field, %s AS label, COUNT(*) AS count FROM clicks WHERE user={:u} AND %s!='' GROUP BY %s ORDER BY count DESC LIMIT 5)",
+			f[0], f[1], f[1], f[1],
+		)
+	}
 	type row struct {
 		Field string `db:"field"`
 		Label string `db:"label"`
 		Count int    `db:"count"`
 	}
 	var rows []row
-	db.NewQuery(`
-		WITH
-		  countries AS (SELECT country_name AS label, COUNT(*) AS count FROM clicks WHERE user = {:u} AND country_name != '' GROUP BY country_name ORDER BY count DESC LIMIT 5),
-		  devices   AS (SELECT device,               COUNT(*) AS count FROM clicks WHERE user = {:u} AND device        != '' GROUP BY device        ORDER BY count DESC LIMIT 5),
-		  referrers AS (SELECT referrer,             COUNT(*) AS count FROM clicks WHERE user = {:u} AND referrer      != '' GROUP BY referrer      ORDER BY count DESC LIMIT 5),
-		  browsers  AS (SELECT browser,              COUNT(*) AS count FROM clicks WHERE user = {:u} AND browser       != '' GROUP BY browser       ORDER BY count DESC LIMIT 5),
-		  os        AS (SELECT os,                   COUNT(*) AS count FROM clicks WHERE user = {:u} AND os            != '' GROUP BY os            ORDER BY count DESC LIMIT 5),
-		  languages AS (SELECT language,             COUNT(*) AS count FROM clicks WHERE user = {:u} AND language      != '' GROUP BY language      ORDER BY count DESC LIMIT 5)
-		SELECT 'country'  AS field, label, count FROM countries
-		UNION ALL
-		SELECT 'device',            label, count FROM devices
-		UNION ALL
-		SELECT 'referrer',          label, count FROM referrers
-		UNION ALL
-		SELECT 'browser',           label, count FROM browsers
-		UNION ALL
-		SELECT 'os',                label, count FROM os
-		UNION ALL
-		SELECT 'language',          label, count FROM languages
-	`).Bind(dbx.Params{"u": uid}).All(&rows)
-
+	db.NewQuery(strings.Join(parts, " UNION ALL ")).
+		Bind(dbx.Params{"u": uid}).All(&rows)
 	bd := breakdownData{
 		Countries: []breakdownItem{},
 		Devices:   []breakdownItem{},
