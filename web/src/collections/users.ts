ef71cb2e0b3c869162f9c8a717eac2859bdc1be9
Shihab Mahamud <shihab4t@gmail.com>
2026-05-14 08:34:14 +0600

    redirect to the root page

--- web/src/collections/users.ts
+++ web/src/collections/users.ts
@@ -17,7 +17,7 @@ export const refresh = async () => {
 
 export const signOut = () => {
     pb.authStore.clear();
-    window.location.reload();
+    window.location.href = '/';
 };
 
 export const getAuth = (): TUser | null => {
