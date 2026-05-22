5619f7995bb2df12c5975fcea0c832aaf103c45f
Shihab Mahamud <shihab4t@gmail.com>
2026-05-23 04:55:06 +0600

    wip

--- web/src/routes/dashboard/_auth/links/index.tsx
+++ web/src/routes/dashboard/_auth/links/index.tsx
@@ -1,4 +1,5 @@
 import type { TFilter } from '#/types/utils';
+import type { ReactNode } from 'react';
 
 import { filterLinks } from '#/lib/links';
 import { getLinksData } from '#/services/links';
@@ -6,6 +7,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { useState } from 'react';
 
 import { RefreshButton } from '#/components/composite/refresh-button';
+import { RouteError } from '#/components/composite/route-error';
 import { Header } from '#/components/composite/site-header';
 import { CreateLinkButton } from '#/components/screens/links/create-link-button';
 import { FiltersTabs } from '#/components/screens/links/index/filters-tabs';
@@ -17,43 +19,58 @@ export const Route = createFileRoute('/dashboard/_auth/links/')({
     head: () => ({ meta: [{ title: 'Links — urlz' }] }),
     loader: () => getLinksData(),
     pendingComponent: () => (
-        <>
-            <Header title="Links" description="Manage and monitor all your shortened links" />
-            <div className="flex flex-col gap-4 p-4 lg:p-6">
-                <Loading />
-            </div>
-        </>
+        <Layout refreshDisable>
+            <Loading />
+        </Layout>
     ),
-    component: Links,
+    errorComponent: ({ error }) => (
+        <Layout>
+            <RouteError error={error} />
+        </Layout>
+    ),
+    component: RouteComponent,
 });
 
-function Links() {
-    const data = Route.useLoaderData();
-
-    const [filter, setFilter] = useState<TFilter>('all');
-    const [search, setSearch] = useState('');
-
-    const links = data?.links ?? [];
-
+function Layout({
+    children,
+    refreshDisable = false,
+}: {
+    children: ReactNode;
+    refreshDisable?: boolean;
+}) {
     return (
         <>
             <Header
                 title="Links"
                 description="Manage and monitor all your shortened links"
                 action={
-                    <div className="flex items-center gap-2">
-                        <RefreshButton />
-                        <CreateLinkButton />
-                    </div>
+                    !refreshDisable && (
+                        <div className="flex items-center gap-2">
+                            <RefreshButton />
+                            <CreateLinkButton />
+                        </div>
+                    )
                 }
             />
-            <div className="flex flex-col gap-4 p-4 lg:p-6">
-                <div className="flex flex-wrap items-center gap-3">
-                    <SearchBox search={search} onSearch={setSearch} />
-                    <FiltersTabs links={links} filter={filter} onFilter={setFilter} />
-                </div>
-                <LinksTable links={filterLinks({ links, search, filter })} />
-            </div>
+            <div className="flex flex-col gap-4 p-4 lg:p-6">{children}</div>
         </>
+    );
+}
+
+function RouteComponent() {
+    const data = Route.useLoaderData();
+
+    const [filter, setFilter] = useState<TFilter>('all');
+    const [search, setSearch] = useState('');
+    const links = data?.links ?? [];
+
+    return (
+        <Layout>
+            <div className="flex flex-wrap items-center gap-3">
+                <SearchBox search={search} onSearch={setSearch} />
+                <FiltersTabs links={links} filter={filter} onFilter={setFilter} />
+            </div>
+            <LinksTable links={filterLinks({ links, search, filter })} />
+        </Layout>
     );
 }
